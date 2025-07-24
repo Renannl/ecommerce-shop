3c49ba78e14574d3eddd808114d9fb89974e2c5a
Renan <mxgames02@gmail.com>
2025-07-23 22:34:47 -0300

    add itens to cart

--- js/cart.js
+++ js/cart.js
@@ -47,6 +47,7 @@ btnAddToCart.forEach((button) => {
     }
     saveCart(cart)
     updateCartCounter()
+    renderCartTable()
     })
 })
 
@@ -56,13 +57,37 @@ function getProductsCart(){
 }
 
 function updateCartCounter(){
-    const cart = getProductsCart()
+    const products = getProductsCart()
     let total = 0
 
-    cart.forEach(product => {
+    products.forEach(product => {
         total += product.quantity
     })
     document.getElementById("cart-counter").textContent = total
 }
 
 updateCartCounter()
+
+function renderCartTable(){
+    const products = getProductsCart()
+    const tableBody = document.querySelector("#modal-1-content table tbody")
+    tableBody.innerHTML= ""
+
+    products.forEach(product => {
+        const tr = document.createElement("tr")
+        tr.innerHTML = `<td class="td-product">
+                                <img src="${product.image}" alt="${product.name}">
+                            </td>
+                            <td>${product.name}</td>
+                            <td class="td-price-unitary">$${product.price}</td>
+                            <td class="td-quantity">
+                                <input type="number" value="${product.quantity}" min="1">
+                            </td>
+                            <td class="td-total-price">$${product.price}</td>
+                            <td>
+                                <button class="btn-remove" data-id="${product.id}"></button>
+                            </td>`
+        tableBody.appendChild(tr)
+    })
+}
+renderCartTable()
